94d7285adec78e134793697c9116a8bde1e4f6c2
elhajuojy <souirimehdi311@gmail.com>
2022-08-24 13:35:34 +0100

    color

--- lib/core/color.dart
+++ lib/core/color.dart
@@ -3,5 +3,6 @@ import 'package:flutter/cupertino.dart';
 class AppColor {
   static Color primaryGreenColor = const Color(0xFF016A40);
   static Color primaryDarkColor = const Color(0xFF171514);
+  static Color SecondDarkColor = Color.fromARGB(255, 44, 41, 40);
   static Color primaryLightColor = Color.fromARGB(255, 244, 237, 237);
 }
